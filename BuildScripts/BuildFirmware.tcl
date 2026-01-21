645930a356c5dcc5630ce1a68b4d7b65e5e543fb
Michael Davidsaver <mdavidsaver@gmail.com>
2026-01-20 19:23:36 -0800

    BuildFirmware handle missing DCP

--- BuildScripts/BuildFirmware.tcl
+++ BuildScripts/BuildFirmware.tcl
@@ -1,5 +1,17 @@
 open_project EVG.xpr
 
+# The first time through, the DCP files are not present.
+# However, vivado will barf if they are still referenced :(
+# Will be automatically re-added after a successful run.
+foreach dcp [get_files -quiet -filter file_type=="Design\ Checkpoint"] {
+    if {[get_property IS_AVAILABLE $dcp] == 0} {
+        puts "Remove reference to missing DCP $dcp"
+        remove_files $dcp
+    } else {
+        puts "Found DCP: $dcp"
+    }
+}
+
 reset_run synth_1
 launch_runs -jobs 2 synth_1
 wait_on_run synth_1
