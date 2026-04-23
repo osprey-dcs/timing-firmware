bb1aad0c81dbb3d13e1ff2628a7b5f2a6ca7be44
Eric Norum <enorum@ospreydcs.com>
2026-04-23 14:49:19 -0700

    Move ADS7253 readout to firmware.  Lowers overhead for this much more common operation.

--- EVG.srcs/sources_1/hdl/ospreyRFIN.v
+++ EVG.srcs/sources_1/hdl/ospreyRFIN.v
@@ -23,40 +23,109 @@
  */
 
 /*
- * Bit-bang interface to RF-IN FMC module
+ * Bit-bang interface to control Osprey RF-IN FMC module devices.
+ * Firmware readout of input level monitoring ADCs.
  */
 `default_nettype none
 module ospreyRFIN #(
-    parameter DEBUG = "false"
+    parameter SYSCLK_RATE = 100000000,
+    parameter ADCCLK_RATE = 1000000,
+    parameter DEBUG       = "false"
     ) (
     input  wire        sysClk,
     input  wire        csrStrobe,
     input  wire [31:0] GPIO_OUT,
-    output wire [31:0] status,
+    output reg  [31:0] readback = 0,
 
-    output wire RFIN_LMK01801_CLK,
-    output wire RFIN_LMK01801_LE,
-    output wire RFIN_LMK01801_DATA,
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_LMK01801_CLK,
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_LMK01801_LE,
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_LMK01801_DATA,
 
-    output wire RFIN_ADS7253_CLK,
-    output wire RFIN_ADS7253_CSB,
-    output wire RFIN_ADS7253_DIN,
-    input  wire RFIN_ADS7253_DOUTA,
-    input  wire RFIN_ADS7253_DOUTB);
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_ADS7253_CLK,
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_ADS7253_CSB = 1,
+    (*MARK_DEBUG=DEBUG*) output reg  RFIN_ADS7253_DIN,
+    (*MARK_DEBUG=DEBUG*) input  wire RFIN_ADS7253_DOUTA,
+    (*MARK_DEBUG=DEBUG*) input  wire RFIN_ADS7253_DOUTB);
 
-(*MARK_DEBUG=DEBUG*) reg [5:0] fmcBits = 0;
+localparam TICK_DIVISOR = ((SYSCLK_RATE/2) + ADCCLK_RATE - 1) / ADCCLK_RATE;
+localparam TICK_COUNTER_LOAD = TICK_DIVISOR - 2;
+localparam TICK_COUNTER_WIDTH = $clog2(TICK_COUNTER_LOAD+1) + 1;
+
+localparam BIT_COUNTER_LOAD = 32 - 2;
+localparam BIT_COUNTER_WIDTH = $clog2(BIT_COUNTER_LOAD+1) + 1;
+
+reg [TICK_COUNTER_WIDTH-1:0] tickCounter = TICK_COUNTER_LOAD;
+(*MARK_DEBUG=DEBUG*) wire tick = tickCounter[TICK_COUNTER_WIDTH-1];
+
+reg [BIT_COUNTER_WIDTH-1:0] bitCounter = BIT_COUNTER_LOAD;
+(*MARK_DEBUG=DEBUG*) wire bitCounterDone = bitCounter[BIT_COUNTER_WIDTH-1];
+
+reg [15:0] shiftA, shiftB;
+
+(*MARK_DEBUG=DEBUG*) reg adcStop = 0, adcStart = 0, adcStopped = 0;
+reg sysADCclk = 0, sysADCcsb = 1, sysADCdin = 0;
 
 always @(posedge sysClk) begin
-    if (csrStrobe) fmcBits <= GPIO_OUT[5:0];
+    if (csrStrobe) begin
+        RFIN_LMK01801_DATA <=  GPIO_OUT[7];
+        RFIN_LMK01801_LE   <=  GPIO_OUT[6];
+        RFIN_LMK01801_CLK  <=  GPIO_OUT[5];
+        sysADCdin          <=  GPIO_OUT[4];
+        sysADCcsb          <= !GPIO_OUT[3];
+        sysADCclk          <=  GPIO_OUT[2];
+        adcStart           <=  GPIO_OUT[1];
+        adcStop            <=  GPIO_OUT[0];
+    end
+
+    /*
+     * ADC clock timing
+     */
+    if (tick) begin
+        tickCounter <= TICK_COUNTER_LOAD;
+    end
+    else begin
+        tickCounter <= tickCounter - 1;
+    end
+
+    /*
+     * ADC control/readout
+     */
+    if (adcStopped) begin
+        if (adcStart) begin
+            adcStopped <= 0;
+        end
+        RFIN_ADS7253_CLK <= sysADCclk;
+        RFIN_ADS7253_CSB <= sysADCcsb;
+        RFIN_ADS7253_DIN <= sysADCdin;
+        readback <= {{29{1'b0}}, RFIN_ADS7253_DOUTB, RFIN_ADS7253_DOUTA, 1'b1};
+    end
+    else if (tick) begin
+        RFIN_ADS7253_CLK <= !RFIN_ADS7253_CLK;
+        RFIN_ADS7253_DIN <= 0;
+        if (RFIN_ADS7253_CSB) begin
+            bitCounter <= BIT_COUNTER_LOAD;
+            if (adcStop) begin
+                adcStopped <= 1;
+            end
+            else if (!RFIN_ADS7253_CLK) begin
+                RFIN_ADS7253_CSB <= 0;
+            end
+        end
+        else begin
+            if (RFIN_ADS7253_CLK) begin
+                shiftA <= {shiftA[14:0], RFIN_ADS7253_DOUTA};
+                shiftB <= {shiftB[14:0], RFIN_ADS7253_DOUTB};
+            end
+            else begin
+                bitCounter <= bitCounter - 1;
+                if (bitCounterDone) begin
+                    RFIN_ADS7253_CSB <= 1;
+                    readback <= {shiftA, shiftB[15:1], 1'b0};
+                end
+            end
+        end
+    end
 end
 
-assign RFIN_ADS7253_CLK   =  fmcBits[0];
-assign RFIN_ADS7253_CSB   = !fmcBits[1];
-assign RFIN_ADS7253_DIN   =  fmcBits[2];
-assign RFIN_LMK01801_CLK  =  fmcBits[3]; 
-assign RFIN_LMK01801_LE   =  fmcBits[4]; 
-assign RFIN_LMK01801_DATA =  fmcBits[5]; 
-
-assign status = { {30{1'b0}}, RFIN_ADS7253_DOUTB, RFIN_ADS7253_DOUTA };
 endmodule
 `default_nettype wire
