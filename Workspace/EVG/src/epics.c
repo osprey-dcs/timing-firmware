osprey-dcs/timing-firmware

commit 4e58fb265dedb19a75c8ffb6855e0699d011cc4c

remove systemParameters.mgtClkSwitch0
Boot to MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0
aka. internal 125MHz

Workspace/EVG/src/epics.c
@@ -104,13 +104,12 @@ struct LEEPpacket {
 #define RANGE(base, count) (base) ... ((base)+(count)-1)
 
 static int powerUpFlag = 1;
+// default must match mgtClkSwitch.c
+static int mgtClkSwitch0 = MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0;
 
 void
 setMgtClkSwitch0(int inputClkIndex)
 {
-    uint32_t now;
-    static uint32_t whenWritten;
-    static int writeCount;
     if ((inputClkIndex < 0)
      || (inputClkIndex >= MGT_CLK_SWITCH_INPUT_DISABLE_OUTPUT)) {
         return;
@@ -121,11 +120,10 @@ setMgtClkSwitch0(int inputClkIndex)
         printf("WARNING -- Setting MGT clock to FMC1 GBTCLK but "
                "RF-IN mezzanine card is not present!\n");
     }
-    if ((whenWritten == 0)
-     || (inputClkIndex != systemParameters.mgtClkSwitch0)) {
+    if (inputClkIndex != mgtClkSwitch0) {
         mgtClkSwitchConnectOutputToInput(MGT_CLK_SWITCH_OUTPUT_MGTCLK0,
                                          inputClkIndex);
-        if ((whenWritten == 0) || (debugFlags & DEBUGFLAG_EPICS_WRITE)) {
+        if (debugFlags & DEBUGFLAG_EPICS_WRITE) {
             printf("Drive MGT clock from ");
             switch(inputClkIndex) {
             case MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0:
@@ -138,20 +136,7 @@ setMgtClkSwitch0(int inputClkIndex)
             }
             printf(".\n");
         }
-        systemParameters.mgtClkSwitch0 = inputClkIndex;
-        now = GPIO_READ(GPIO_IDX_SECONDS_SINCE_BOOT);
-        if ((now - whenWritten) > 120) {
-            writeCount = 0;
-        }
-        else {
-            writeCount++;
-        }
-        if (writeCount >= 5) {
-            printf("Warning -- MGT clock reference changing rapidly.  "
-                   "Risk of flash wear.\n");
-        }
-        systemParametersStash();
-        whenWritten = now;
+        mgtClkSwitch0 = inputClkIndex;
     }
 }
 
@@ -238,7 +223,7 @@ readReg(int address)
         }
         }
         return 0;
-    case REG_MARBLE_MGT_REFCLK_SOURCE: return systemParameters.mgtClkSwitch0;
+    case REG_MARBLE_MGT_REFCLK_SOURCE: return mgtClkSwitch0;
     case REG_MARBLE_PPS_LOCAL_CSR:     return localPPSstatus();
     }
 

Workspace/EVG/src/mgtClkSwitch.c
@@ -92,7 +92,7 @@ mgtClkSwitchInit(void)
     for (outputIndex = 0 ; outputIndex < 8 ; outputIndex++) {
         outputEnable(outputIndex, 0);
     }
-    setMgtClkSwitch0(systemParameters.mgtClkSwitch0);
+    setMgtClkSwitch0(MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0);
 }
 
 void

Workspace/EVG/src/systemParameters.c
@@ -93,7 +93,8 @@ systemParametersInit(void)
     if (fr != FR_OK) {
         printf("Assigning default parameters:\n");
         systemParameters.startupDebugFlags = 0;
-        systemParameters.mgtClkSwitch0 = MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0;
+        // preserve previous default for now unused mgtClkSwitch0
+        systemParameters.unused1 = MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0;
         systemParameters.netmask = 0xFFFFFF00;
         systemParameters.gateway = (networkConfig.ipv4address &
                                                   systemParameters.netmask) | 1;

Workspace/EVG/src/systemParameters.h
@@ -36,7 +36,7 @@
  */
 struct systemParameters {
     unsigned int startupDebugFlags;
-    int          mgtClkSwitch0;
+    int          unused1; // formerly mgtClkSwitch0
     uint32_t     netmask;
     uint32_t     gateway;
     uint32_t     ntpServer;

commit 21df79b7aeca1e9f1a1b7a4d5657c7c743ae13fc

correctly initialize mgtClkSwitch0
Delta test must always allow first set

regression in 4e58fb265dedb19a75c8ffb6855e0699d011cc4c

Workspace/EVG/src/epics.c
@@ -104,8 +104,7 @@ struct LEEPpacket {
 #define RANGE(base, count) (base) ... ((base)+(count)-1)
 
 static int powerUpFlag = 1;
-// default must match mgtClkSwitch.c
-static int mgtClkSwitch0 = MGT_CLK_SWITCH_INPUT_FPGA_REF_CLK0;
+static int mgtClkSwitch0 = -1; // spoil
 
 void
 setMgtClkSwitch0(int inputClkIndex)